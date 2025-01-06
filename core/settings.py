f630efa8d231b073ce93f8496b56060c9c3c19c0
Caio Carvalho <caiocarvalho.py@gmail.com>
2025-01-05 22:47:27 -0400

    Debug and StaticFiles

--- core/settings.py
+++ core/settings.py
@@ -5,7 +5,7 @@ from dj_database_url import parse as db_url
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
-DEBUG = config('DEBUG', default=True, cast=bool)
+DEBUG = config('DEBUG', default=False, cast=bool)
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='127.0.0.1', cast=Csv())
 
 
@@ -94,6 +94,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = 'core/staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
